58c12d62e83e55dc46af81b13fe25ce5a4d475b2
Alex Ramponi <alex.a.ramponi@gmail.com>
2020-02-04 22:53:16 -0600

    Session almost working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,20 +14,46 @@ class MoviesController < ApplicationController
     #Load all the ratings from the movie class
     @all_ratings = Movie.ratings
 
-    #Sort problem
-    @sort = params[:sort]
 
-    #If there are params sent we filter out the ratings also
+    #Need this for the redirect URI to remain restful
+    loaded_session = false
+
+    #Sort problem
+    if params[:sort]
+      #If we have a param for sort, we want to set sort to this as well as update the session
+      @sort = params[:sort]
+      session[:sort] = params[:sort]
+    elsif session[:sort]
+      #If there isn't a param but there's a session, we want to load that
+      @sort = session[:sort]
+      loaded_session = true
+    else
+      #If neither then no sort so set to nil for query
+      @sort = nil
+    end
+
+      
     if params[:ratings]
       #use this to keep track in the index view
       @checked_boxes = params[:ratings]
       @movies = Movie.where(rating: @checked_boxes.keys).order(@sort)
-      #puts params[:ratings].keys
+      session[:ratings] = params[:ratings]
+    elsif session[:ratings]
+      @checked_boxes = session[:ratings]
+      @movies = Movie.where(rating: @checked_boxes.keys).order(@sort)
+      loaded_session = true
     else #If not just stick to the sort stuff
-      #sets up the nil case
+      #sets up the nil case when we have a new program
       @checked_boxes = []
       @movies = Movie.all.order(@sort)
     end
+
+    #If one is nil we redirect
+    if loaded_session 
+     flash.keep
+     redirect_to movies_path(:sort=>@sort, :ratings=>@checked_boxes)
+    end
+
     @movies
   end
 
